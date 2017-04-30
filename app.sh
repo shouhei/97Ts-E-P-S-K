10a65ea15e6404b07a22a651de77ae687b52338c
山口将平 <shouhei.yamaguchi.be@gmail.com>
2017-05-01 00:30:04 +0900

    fix path

--- app.sh
+++ app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cwd=`dirname "${0}"`
-curl `cat $cwd/url` -d "payload={\"text\": \"本日の1日1エッセイ\n http://プログラマが知るべき97のこと.com/`cat $cwd/titles | shuf -n 1`\"}"
+curl `cat $cwd/url` -d "payload={\"text\": \"本日の1日1エッセイ\n http://プログラマが知るべき97のこと.com/エッセイ/`cat $cwd/titles | shuf -n 1`\"}"
